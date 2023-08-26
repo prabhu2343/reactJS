637b2f74a3e84d6ef7e38b06b492f2fd4cdbf47c
prabhu2343 <prabhu.ramdrums@gmail.com>
2023-08-26 09:53:11 +0530

    spell change testing

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ function Header(){
                     <li><a href="#">About</a></li>
                     <li><a href="#">Gallery</a></li>
                     <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Contact Us</a></li>
                 </ul>
             </nav>
         </header>
